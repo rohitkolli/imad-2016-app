fe1e7e063b701e6b8d526cd155b22e6e6e5e1e33
rohitkolli <ks.rohit96@gmail.com>
2016-10-01 10:48:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/rohit', function (req, res) {
+  res.send("u are in /rohit url");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
